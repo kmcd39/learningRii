000b30103121de26f4f77a0f5c5dbed6107ebe45
Kira McDonald <kira.mcd.99@gmail.com>
2024-10-29 20:35:54 -0400

    added code for basic ggplot

--- semester i/ggplot and census.R
+++ semester i/ggplot and census.R
@@ -35,9 +35,44 @@ ny.census <-
   ,get.demographics.and.commute = T
   )
 
+
+# peeks -------------------------------------------------------------------
+
 ny.census
+ny.census %>% colnames()
 ny.census %>% glimpse()
 
+ny.census <- ny.census %>%
+  mutate(boro = substr(geoid, 1, 5))
+
+# The Bronx is Bronx County (ANSI / FIPS 36005)
+# Brooklyn is Kings County (ANSI / FIPS 36047)
+# Manhattan is New York County (ANSI / FIPS 36061)
+# Queens is Queens County (ANSI / FIPS 36081)
+# Staten Island is Richmond County (ANSI / FIPS 36085)
+
+# brainstorm uses of the data ---------------------------------------------
+
+#' what is amount of affordable housing?
+#'
+#' let's do a distribution of median housing costs per neighborhood.
+
+
+ny.census %>%
+  ggplot(
+    aes(
+       x =
+         #med.hcosts
+          med.crent
+      ,fill = factor(boro)
+      ,weight = pop
+      )
+  ) +
+  geom_histogram(
+    binwidth = 500
+  ) +
+  facet_wrap(vars(boro)
+             )
 
 
 
