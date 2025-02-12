8c3bd0ae275db75e9e7795baecb337ffb6f5f023
Kira McDonald <kira.mcd.99@gmail.com>
2025-02-11 20:52:39 -0500

    end of class microdata and weights

--- semester ii/Microdata and weights.R
+++ semester ii/Microdata and weights.R
@@ -53,23 +53,42 @@ labls
 state.populations
 
 # let's match the labels to the actual data
-state.populations %>%
+state.populations <-
+  state.populations %>%
   rename(state = NAME) %>%
   left_join(labls
             ,by = c("variable" = "name")
-            )
+            ) %>%
+  select(-concept, -geography)
+
+# now we have labels!
+state.populations
+
+# can we separate out gender/age from the labels?
+# state.populations %>% count(label) %>% View()
+state.populations %>%
+  mutate(
+    gender =
+      str_extract(label, "Female|Male")
+    ,age.bucket =
+      str_extract(label, "\\d.*$|Under.*$")
+  )
+
+
+# let's think a moment about how this data was actually put together...
+al.populations
+state.populations
 
 # -------------------------------------------------------------------------
 
 # peek at avialbel microdata variables
-tidycensus::pums_variables %>% count(year)
 tidycensus::pums_variables %>%
   filter(year == 2022) %>%
   filter(survey == "acs1") %>%
   View( )
 
 tidycensus::pums_variables %>%
-  filter(variable %in%
+  filter(var_code %in%
            c("TEN", "HHL"))
 
 #' use tidycensus to pull some microdata
@@ -77,30 +96,46 @@ tidycensus::pums_variables %>%
 #' Census bureau has documentaiton on PUMS areas
 #'
 #' https://www.census.gov/geographies/reference-maps/2010/geo/2010-pumas/new-york.html
-pums <- tidycensus::get_pums(
+#'
+#' and a resource from the person who wrote the R package --
+#' https://walker-data.com/tidycensus/articles/pums-data.html
+pums <-
+  tidycensus::get_pums(
   state = "36"
   ,puma = "03807" # NYC-Manhattan Community District 4 & 5--Chelsea, Clinton & Midtown Business District PUMA
   ,year = 2021
   ,survey = "acs1"
-  ,variables = c("TEN", "HHL")
+  ,variables = c("TEN", "HHL", "AGEP")
   ,rep_weights = "housing"
   ,recode = T
 )
 
 
-pums %>%
-  glimpse()
+pums %>% glimpse()
+pums$SERIALNO %>% str_extract("[A-Z]+") %>% unique()
+pums$SERIALNO %>% head()
 
+pums
 
 # a quick analysis -- -----------------------------------------------------
 
+pums$WGTP %>% sum()
 
+# WE can get estimates by summing the weights
 tmp <- pums %>%
   group_by(TEN, TEN_label,
-           HHL, HHL_label) %>%
+           HHL, HHL_label
+           ) %>%
   summarise(estimate = sum(WGTP)
             ) %>%
   ungroup()
+
+tmp
+
+# BUT that doesn't give us margins of error, which is very important!
+
+pums %>%
+
 
 tmp %>%
   ggplot() #......
